191cbedfacd1e917dcc83c209b5a4da22a14e4ec
BaashaXclouD <96932663+BaashaXclouD@users.noreply.github.com>
2022-12-24 21:49:20 +0530

    Mm

--- start.sh
+++ start.sh
@@ -1,12 +1,4 @@
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://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@github.com/phoenixtgbot/FilterBot /main
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Eva
-fi
-cd /main
+cd FilterBot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
