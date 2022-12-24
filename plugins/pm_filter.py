339270b2806d0ff9cbc474a0bd941fa7a2121d57
BaashaXclouD <96932663+BaashaXclouD@users.noreply.github.com>
2022-12-24 21:57:17 +0530

    Inline

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -387,8 +387,15 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 await query.answer(url=f"https://t.me/{temp.U_NAME}?start={ident}_{file_id}")
                 return
             else:
+                shortbtn = [
+                    [
+                        InlineKeyboardButton(text=f"Download Link", url=f"{shorten_link}")
+                        InlineKeyboardButton(text=f"How To Downlload", url=f"{H_DOWNLOAD_LINK}")
+                    ]
+                ]
+                short_markup = InlineKeyboardMarkup(shortbtn)
                 shorten_link = short_url(f"https://t.me/{temp.U_NAME}?start={ident}_{file_id}")
-                await client.send_message(chat_id=query.from_user.id,text = f"<b>==> Title : {files.file_name}\n\n==> File_Size : {get_size(files.file_size)}\n\n==> Download Link : {shorten_link}\n\n==>How To Download : {H_DOWNLOAD_LINK}</b>", disable_web_page_preview=True)
+                await client.send_message(chat_id=query.from_user.id,text = f"<b>==> Title : {files.file_name}\n\n==> File_Size : {get_size(files.file_size)}, reply_markup=short_markup)
                 await query.answer('Check PM, I have sent files in pm', show_alert=True)
         except UserIsBlocked:
             await query.answer('You Are Blocked to use me !', show_alert=True)
